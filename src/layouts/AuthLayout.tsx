9fa5546dea43544c15644da323317e6293d318de
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-07 18:38:29 +0500

    protected the routes

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,6 +1,14 @@
 import { Outlet } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import useTokenStore from "@/store";
 
 const AuthLayout = () => {
+  const token = useTokenStore((state) => state.token);
+
+  if (token) {
+    return <Navigate to={"/dashboard/home"} replace />;
+  }
+
   return (
     <>
       <Outlet />
